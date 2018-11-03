c969ba82de1e8c0a88e6efd82f6423edcc7e1512
Mike Reza <mikerza@gmail.com>
2018-11-03 00:03:33 -0500

    routing fix

--- app/server.js
+++ app/server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 
-require('../app/routing/apiRoutes.js')(app); 
-require('../app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
 
 
 app.listen(PORT, function(){
