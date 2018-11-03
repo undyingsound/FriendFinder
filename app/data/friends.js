06c51cf19a23417d2a0ebbb184c6f46e9af54c53
Mike Reza <mikerza@gmail.com>
2018-11-03 02:51:10 -0500

    hard-coded matches

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,78 @@
-let matches =[];
+let matches =[
+    {
+    "name": "Mike",
+    "photo": "https://undyingsound.github.io/Bootstrap-Portfolio/assets/images/mainpic.png",
+    "gender": "male",
+    "scores": [
+    5,
+    4,
+    4,
+    4,
+    4,
+    4,
+    4,
+    5,
+    5,
+    3
+    ]
+    },
+    {
+        "name": "Alice",
+        "photo": "https://i.redd.it/ahp1kefp8ch01.jpg",
+        "gender": "female",
+        "scores": [
+        3,
+        4,
+        3,
+        4,
+        4,
+        3,
+        4,
+        5,
+        2,
+        1
+        ]
+        },
+        {
+        "name": "Sofia",
+        "photo": "http://www.fgxi.com/wp-content/uploads/sites/24/2015/04/img-sofia-vergara-portrait.jpg",
+        "gender": "female",
+        "scores": [
+        5,
+        4,
+        4,
+        4,
+        4,
+        5,
+        4,
+        5,
+        4,
+        1
+        ]
+        }, 
+        {
+            "name": "Chad",
+            "photo": "https://pbs.twimg.com/media/BH7mQBoCIAAbNbo.jpg",
+            "gender": "male",
+            "scores": [
+            1,
+            5,
+            5,
+            1,
+            1,
+            5,
+            1,
+            1,
+            1,
+            5
+            ]
+            }
+
+
+
+
+
+]
 
 module.exports = matches;
 
