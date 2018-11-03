87373916f56419bda554a04f6f47be97a9c7a541
Mike Reza <mikerza@gmail.com>
2018-11-02 23:29:56 -0500

    pkg adjust

--- app/server.js
+++ app/server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
+app.use(express.static('app'));
 
 require('../app/routing/apiRoutes')(app); 
 require('../app/routing/htmlRoutes')(app);
