d791f9499db2c46d0ad5f3a0a620474b3e67c4fa
Mike Reza <mikerza@gmail.com>
2018-11-02 23:46:35 -0500

    pkg adjust

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(express.static('app'));
 
-require('./app/routing/apiRoutes')(app); 
-require('./app/routing/htmlRoutes')(app);
+require('../app/routing/apiRoutes')(app); 
+require('../app/routing/htmlRoutes')(app);
 
 
 app.listen(PORT, function(){
